d246dfb49e739b4c720c6b959a125327e7450567
katrinDY <katrin.yordanova@crust.tech>
2026-05-24 20:52:31 +0300

    Add type to transactions schema

--- db/migrations/002_create_transactions.sql
+++ db/migrations/002_create_transactions.sql
@@ -1,7 +1,14 @@
+DO $$ BEGIN
+  CREATE TYPE transaction_type AS ENUM ('income', 'expense');
+EXCEPTION
+  WHEN duplicate_object THEN NULL;
+END $$;
+
 CREATE TABLE IF NOT EXISTS transactions (
   id SERIAL PRIMARY KEY,
   user_id INT NOT NULL,
   amount DECIMAL(10, 2) NOT NULL,
+  type transaction_type NOT NULL,
   category VARCHAR(255) NOT NULL,
   description TEXT,
   date TIMESTAMPTZ NOT NULL DEFAULT NOW(),
